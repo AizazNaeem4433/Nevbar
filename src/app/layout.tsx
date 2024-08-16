1b6e0cc64cff8c2829a8648142c0e9ed6c604b93
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2024-08-17 03:46:05 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import StarsCanvas from "@/components/Starbackground";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Nevbar",
+  title: "Aizaz",
   description: "NevBar testings",
 };
 
